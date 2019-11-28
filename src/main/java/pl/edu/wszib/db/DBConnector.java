cc979ad93bc8b644a34bff652678de888a59e7b4
github.com (140.82.118.3) <modryyyhd@gmail.com>
2019-11-28 19:43:41 +0100

    update

--- src/main/java/pl/edu/wszib/db/DBConnector.java
+++ src/main/java/pl/edu/wszib/db/DBConnector.java
@@ -4,7 +4,7 @@ package pl.edu.wszib.db;
         import org.hibernate.cfg.Configuration;
         import pl.edu.wszib.model.Pytanie;
 
-        import javax.persistence.EntityManager;
+
         import java.util.List;
 
 public class DBConnector {
@@ -58,14 +58,13 @@ public class DBConnector {
     }
 
 
-    public static void updateQuestion(int idPytania, Pytanie pytanie) {
+    public static void updateWholeQuestion(int idPytania, Pytanie pytanie) {
         Session session = factory.openSession();
         Transaction tx = null;
-        Pytanie pytanie1;
+
         try {
             tx = session.beginTransaction();
-            pytanie1 = (Pytanie) session.load(Pytanie.class, idPytania);
-            System.out.println(pytanie.getIdPytania() +"," + pytanie.getNagroda()+"," + pytanie.getOdpowiedzi()+"," + pytanie.getPrawidlowaOdpowiedz()+"," + pytanie.getPytanie()+"," + pytanie.getClass());
+            session.load(Pytanie.class, idPytania);
             session.saveOrUpdate(pytanie);
             //wiecej operacji
             tx.commit();
@@ -76,6 +75,90 @@ public class DBConnector {
         }
 
 }
+    public static void updateQuestionReward(int idPytania, Pytanie pytanie) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Pytanie pytanie1;
+        try {
+            tx = session.beginTransaction();
+            pytanie1 = (Pytanie) session.load(Pytanie.class, idPytania);
+
+            pytanie1.setNagroda(pytanie.getNagroda());
+
+
+            session.update(pytanie1);
+            //wiecej operacji
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+        } finally {
+            session.close();
+        }
+
+    }
+    public static void updateQuestion(int idPytania, Pytanie pytanie) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Pytanie pytanie1;
+        try {
+            tx = session.beginTransaction();
+            pytanie1 = (Pytanie) session.load(Pytanie.class, idPytania);
+
+            pytanie1.setPytanie(pytanie.getPytanie());
+
+
+            session.update(pytanie1);
+            //wiecej operacji
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+        } finally {
+            session.close();
+        }
+
+    }
+    public static void updateAnswer(int idPytania, Pytanie pytanie) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Pytanie pytanie1;
+        try {
+            tx = session.beginTransaction();
+            pytanie1 = (Pytanie) session.load(Pytanie.class, idPytania);
+
+            pytanie1.setOdpowiedzi(pytanie.getOdpowiedzi());
+
+
+            session.update(pytanie1);
+            //wiecej operacji
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+        } finally {
+            session.close();
+        }
+
+    }
+    public static void updateCorrectAnswer(int idPytania, Pytanie pytanie) {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Pytanie pytanie1;
+        try {
+            tx = session.beginTransaction();
+            pytanie1 = (Pytanie) session.load(Pytanie.class, idPytania);
+
+            pytanie1.setPrawidlowaOdpowiedz(pytanie.getPrawidlowaOdpowiedz());
+
+
+            session.update(pytanie1);
+            //wiecej operacji
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+        } finally {
+            session.close();
+        }
+
+    }
 
 
     public static List<Pytanie> getAllQuestions() {
--- src/main/java/pl/edu/wszib/db/gui/GUI.java
+++ src/main/java/pl/edu/wszib/db/gui/GUI.java
@@ -9,8 +9,7 @@ import java.util.Scanner;
 public class GUI {
         public static void Menu(){
     Scanner scanner = new Scanner(System.in);
-    int wyborIdPytania;
-            System.out.println("1. Dodaj Pytanie, 2.Usun Pytanie, 3.Odczytaj Pytanie, 4. Edytuj Pytanie, 5. Exit, 6. Wszystkie Pytania");
+            System.out.println("1.Dodaj Pytanie \n2.Usun Pytanie \n3.Odczytaj Pytanie \n4.Edytuj Całe Pytanie \n5.Edytuj tylko Nagrodę\n6.Edytuj tylko Pytanie\n7.Edytuj tylko odpowiedzi\n8.Edytuj tylko prawidłową odpowiedź\n9.Wyświetl wszystkie pytania\n10.Wyjdź");
     int wybor = scanner.nextInt();
 
 
@@ -24,23 +23,36 @@ public class GUI {
             Menu();
             break;
         case 3:
-            System.out.println("Podaj id pytania ktore chcesz odczytac: ");
-            wyborIdPytania = scanner.nextInt();
-            Pytanie pytanie = DBConnector.getPytanie(wyborIdPytania);
-            System.out.println(pytanie);
+            OneQuestion();
             Menu();
             break;
         case 4:
 
-            UpdateQuestion();
+            ChangeWholeQuestion();
             Menu();
             break;
         case 5:
-            System.exit(0);
+            ChangeQuestionReward();
+            Menu();
             break;
         case 6:
+            ChangeQuestion();
+            Menu();
+            break;
+        case 7:
+            ChangeAnswer();
+            Menu();
+            break;
+        case 8:
+            ChangeCorrectAnswer();
+            Menu();
+            break;
+        case 9:
             getAllQuestions();
             Menu();
+            break;
+        case 10:
+          System.exit(0);
             break;
         default:
             Menu();
@@ -48,73 +60,55 @@ public class GUI {
     }
 
     }
+    public static void OneQuestion(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj id pytania ktore chcesz odczytac: ");
+        int wyborIdPytania = scanner.nextInt();
+        while(DBConnector.getPytanie(wyborIdPytania)==null) {
+            System.out.println("Podaj jeszcze raz id pytania ktore chcesz odczytac");
+            wyborIdPytania = scanner.nextInt();
+
+        }
+        Pytanie pytanie = DBConnector.getPytanie(wyborIdPytania);
+        System.out.println(pytanie);
+
+    }
 
     public static void AddQuestion(){
-            Pytanie pytanie = new Pytanie();
-            int i =0;
-        boolean prawidlowyWybor = false;
-
-
-        System.out.println("Wybierz nagrode za pytanie!");
-        System.out.println("500, 1000, 2000, 5000, 10000, 20000, 40000, 75000, 125000, 250000, 500000, 1000000");
-        Scanner scanner = new Scanner(System.in);
-        while(prawidlowyWybor == false) {
-           String kwota = scanner.nextLine();
-            try {
-                if (kwota.equals("500") || kwota.equals("1000") || kwota.equals("2000") || kwota.equals("5000") || kwota.equals("10000") ||
-                        kwota.equals("20000") || kwota.equals("40000") || kwota.equals("75000") || kwota.equals("125000") || kwota.equals("250000") ||
-                        kwota.equals("500000") | kwota.equals("1000000")) {
-
-                    pytanie.setNagroda(Integer.parseInt(kwota));
-
-                    prawidlowyWybor = true;
-                }
-                else {System.out.println("podaj prawidlowa wartosc");}
-            }
-            catch (NumberFormatException e) {
-
-            }
-        }
-
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.println("Podaj tresc Pytania");
-        String pyt = scanner1.nextLine();
-        pytanie.setPytanie(pyt);
-
-        Scanner scanner2 = new Scanner(System.in);
-        Scanner scanner3 = new Scanner(System.in);
-        Scanner scanner4 = new Scanner(System.in);
-        Scanner scanner5 = new Scanner(System.in);
-        System.out.println("Podaj proponowe odpowiedzi Pytania");
-        System.out.println("Odpowiedz A: ");
-        String odpA = scanner2.nextLine();
-        System.out.println("Odpowiedz B: ");
-        String odpB = scanner3.nextLine();
-        System.out.println("Odpowiedz C: ");
-        String odpC = scanner4.nextLine();
-        System.out.println("Odpowiedz D: ");
-        String odpD = scanner5.nextLine();
-
-        String Odpowiedz = ("a)"+ odpA +"\t" +"b)"+ odpB +"\t" +"c)"+ odpC +"\t" +"d)"+ odpD);
-        pytanie.setOdpowiedzi(Odpowiedz);
-
-        System.out.println("Ktora odp jest prawidlowa? ");
-        System.out.println("a,b,c czy d");
-
-
-        boolean prawidlowaodp = false;
-        while(prawidlowaodp == false) {
-            String PrawidlowaOdp = scanner.nextLine();
-            if (PrawidlowaOdp.equals("a")|| PrawidlowaOdp.equals("b")||PrawidlowaOdp.equals("c")||PrawidlowaOdp.equals("d")) {
-                pytanie.setPrawidlowaOdpowiedz(PrawidlowaOdp);
-                prawidlowaodp = true;
-            }
-                System.out.println("wpisz poprawnie odpowiedz (tylko 1 litera: a,b,c lub d)");
-
-        }
-        System.out.println("idPytania: "+ i);
+        Pytanie pytanie = new Pytanie();
+        pytanie.setNagroda(AddOrUpdateReward());
+        pytanie.setPytanie(AddOrUpdateQuestion());
+        pytanie.setOdpowiedzi(AddOrUpdateAnswer());
+        pytanie.setPrawidlowaOdpowiedz(AddOrUpdateCorrectAnswer());
+        System.out.println(pytanie);
         DBConnector.addQuestion(pytanie);
+
+
+
+    }
+
+    public static int ID(){
+      //  Pytanie pytanie = new Pytanie();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj idPytania ktorego chcesz aktualizowac: ");
+
+        int idAktualizacja = scanner.nextInt();
+        while (DBConnector.getPytanie(idAktualizacja) == null) {
+            System.out.println("Nie ma takiego pytania! Podaj jeszcze raz idPytania:");
+            idAktualizacja = scanner.nextInt();
+
+
+            System.out.println("Powrot do menu = -10");
+            if (idAktualizacja == -10) {
+                Menu();
+            }
+
         }
+
+        System.out.println(idAktualizacja);
+        return idAktualizacja;
+    }
+
 
     public static void RemoveQuestion() {
 
@@ -131,90 +125,137 @@ public class GUI {
 
             }
         }
+
             DBConnector.removeQuestion(idUsuwanegoPytania);
+
         }
 
-        public static void UpdateQuestion(){
-            Pytanie pytanie = new Pytanie();
 
-            boolean prawidlowyWybor = false;
-            String kwota;
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Podaj idPytania ktorego chcesz aktualizowac: ");
+    public static int AddOrUpdateReward(){
+     //   Pytanie pytanie = new Pytanie();
+        Scanner scanner = new Scanner(System.in);
+        boolean prawidlowyWybor = false;
 
-            int idAktualizacja = scanner.nextInt();
-            while(DBConnector.getPytanie(idAktualizacja) == null){
-                System.out.println("Nie ma takiego pytania! Podaj jeszcze raz idPytania:");
-                idAktualizacja = scanner.nextInt();
-
-                System.out.println("Powrot do menu = -10");
-                if(idAktualizacja == -10){
-                    Menu();
-                }
-
-            }
-
-            pytanie.setIdPytania(idAktualizacja);
 
             System.out.println("Wybierz nagrode za pytanie!");
             System.out.println("500, 1000, 2000, 5000, 10000, 20000, 40000, 75000, 125000, 250000, 500000, 1000000");
 
             while(prawidlowyWybor == false) {
-                kwota = scanner.nextLine();
+                String kwota = scanner.nextLine();
                 try {
                     if (kwota.equals("500") || kwota.equals("1000") || kwota.equals("2000") || kwota.equals("5000") || kwota.equals("10000") ||
                             kwota.equals("20000") || kwota.equals("40000") || kwota.equals("75000") || kwota.equals("125000") || kwota.equals("250000") ||
                             kwota.equals("500000") | kwota.equals("1000000")) {
 
-                        pytanie.setNagroda(Integer.parseInt(kwota));
 
-                        prawidlowyWybor = true;
+                        return Integer.parseInt(kwota);
+
+
                     }
                     else {System.out.println("podaj prawidlowa wartosc");}
                 }
                 catch (NumberFormatException e) {
-
                 }
             }
+            return 0;
+    }
 
-            Scanner scanner1 = new Scanner(System.in);
+        public static String AddOrUpdateQuestion(){
+
+
+            Scanner scanner = new Scanner(System.in);
             System.out.println("Podaj tresc Pytania");
-            String pyt = scanner1.nextLine();
-            pytanie.setPytanie(pyt);
+            String pyt = scanner.nextLine();
 
-            Scanner scanner2 = new Scanner(System.in);
-            Scanner scanner3 = new Scanner(System.in);
-            Scanner scanner4 = new Scanner(System.in);
-            Scanner scanner5 = new Scanner(System.in);
-            System.out.println("Podaj proponowe odpowiedzi Pytania");
-            System.out.println("Odpowiedz A: ");
-            String odpA = scanner2.nextLine();
-            System.out.println("Odpowiedz B: ");
-            String odpB = scanner3.nextLine();
-            System.out.println("Odpowiedz C: ");
-            String odpC = scanner4.nextLine();
-            System.out.println("Odpowiedz D: ");
-            String odpD = scanner5.nextLine();
+            return pyt;
 
-            String Odpowiedz = ("a)"+ odpA +"\t" +"b)"+ odpB +"\t" +"c)"+ odpC +"\t" +"d)"+ odpD);
-            pytanie.setOdpowiedzi(Odpowiedz);
+        }
+    public static String AddOrUpdateAnswer() {
 
-            System.out.println("Ktora odp jest prawidlowa? ");
-            System.out.println("a,b,c czy d");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj proponowe odpowiedzi Pytania");
+        System.out.println("Odpowiedz A: ");
+        String odpA = scanner.nextLine();
+        System.out.println("Odpowiedz B: ");
+        String odpB = scanner.nextLine();
+        System.out.println("Odpowiedz C: ");
+        String odpC = scanner.nextLine();
+        System.out.println("Odpowiedz D: ");
+        String odpD = scanner.nextLine();
+
+        String Odpowiedz = ("a)" + odpA + "\t" + "b)" + odpB + "\t" + "c)" + odpC + "\t" + "d)" + odpD);
+
+ return Odpowiedz;
+    }
+
+    public static String AddOrUpdateCorrectAnswer() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ktora odp jest prawidlowa? ");
+        System.out.println("a,b,c czy d");
 
 
-            boolean prawidlowaodp1 = false;
-            while(prawidlowaodp1 == false) {
-                String PrawidlowaOdp = scanner.nextLine();
-                if (PrawidlowaOdp.equals("a")|| PrawidlowaOdp.equals("b")||PrawidlowaOdp.equals("c")||PrawidlowaOdp.equals("d")) {
-                    pytanie.setPrawidlowaOdpowiedz(PrawidlowaOdp);
-                    break;
-                }
-                System.out.println("wpisz poprawnie odpowiedz (tylko 1 litera: a,b,c lub d)");
+        boolean prawidlowaodp1 = false;
+        while(prawidlowaodp1 == false) {
+            String PrawidlowaOdp = scanner.nextLine();
+            if (PrawidlowaOdp.equals("a")|| PrawidlowaOdp.equals("b")||PrawidlowaOdp.equals("c")||PrawidlowaOdp.equals("d")) {
+
+                System.out.println(PrawidlowaOdp);
+                return PrawidlowaOdp;
 
             }
-            DBConnector.updateQuestion(idAktualizacja, pytanie);
+            System.out.println("wpisz poprawnie odpowiedz (tylko 1 litera: a,b,c lub d)");
+
         }
+
+        System.out.println("error");
+        return null;
+    }
+
+    public static void ChangeWholeQuestion(){
+Pytanie pytanie = new Pytanie();
+int id = ID();
+        pytanie.setIdPytania(id);
+        pytanie.setNagroda(AddOrUpdateReward());
+        pytanie.setPytanie(AddOrUpdateQuestion());
+        pytanie.setOdpowiedzi(AddOrUpdateAnswer());
+        pytanie.setPrawidlowaOdpowiedz(AddOrUpdateCorrectAnswer());
+        System.out.println(pytanie);
+
+            DBConnector.updateWholeQuestion(id, pytanie);
+
+
+    }
+    public static void ChangeQuestionReward(){
+        Pytanie pytanie = new Pytanie();
+        int id = ID();
+        pytanie.setIdPytania(id);
+        pytanie.setNagroda(AddOrUpdateReward());
+        DBConnector.updateQuestionReward(id, pytanie);
+    }
+
+    public static void ChangeQuestion(){
+        Pytanie pytanie = new Pytanie();
+        int id = ID();
+        pytanie.setIdPytania(id);
+        pytanie.setPytanie(AddOrUpdateQuestion());
+        DBConnector.updateQuestion(id, pytanie);
+    }
+    public static void ChangeAnswer(){
+        Pytanie pytanie = new Pytanie();
+        int id = ID();
+        pytanie.setIdPytania(id);
+        pytanie.setOdpowiedzi(AddOrUpdateAnswer());
+        DBConnector.updateAnswer(id, pytanie);
+    }
+    public static void ChangeCorrectAnswer(){
+        Pytanie pytanie = new Pytanie();
+        int id = ID();
+        pytanie.setIdPytania(id);
+        pytanie.setPrawidlowaOdpowiedz(AddOrUpdateCorrectAnswer());
+        DBConnector.updateCorrectAnswer(id, pytanie);
+    }
+
     public static void getAllQuestions(){
     List<Pytanie> allQuestions = DBConnector.getAllQuestions();
         System.out.println("LISTA PYTAN:");
